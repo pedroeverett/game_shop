e4fa36d932d6f7b007acf7aac4084aa20293cc6f
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-16 18:41:58 +0100

    index.erb and app.rb created

---
+++ app.rb
@@ -0,0 +1,8 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('controllers/games_controller.rb')
+require_relative('controllers/publishers_controller.rb')
+
+get ('/') do
+  erb(:index)
+end

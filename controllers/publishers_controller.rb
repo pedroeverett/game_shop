ed91500c89baa70448ca8861486054a41376bc5c
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-17 08:58:14 +0100

    publisher.erb list working

--- controllers/publishers_controller.rb
+++ controllers/publishers_controller.rb
@@ -4,6 +4,7 @@ require('pry-byebug')
 require_relative('../models/game.rb')
 require_relative('../models/publisher.rb')
 
+#index
 get '/publishers' do
   @publishers = Publisher.all()
   erb(:"publishers/index")

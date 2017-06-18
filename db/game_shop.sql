cd189a9733ab30c851e213d952a6504e7706a68d
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-18 08:14:54 +0100

    genre table created

--- db/game_shop.sql
+++ db/game_shop.sql
@@ -1,14 +1,31 @@
 DROP TABLE games;
+DROP TABLE genres;
 DROP TABLE publishers;
 
 CREATE TABLE publishers(
   id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),  
+  city VARCHAR(255),
+  country VARCHAR(255),
+  post_code VARCHAR(255),
+  phone INT(10),
+  website VARCHAR(255),
+  email VARCHAR(255)
+    );
+
+CREATE TABLE genres(
+  id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
-  );
+);
 
 CREATE TABLE games(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   quantity INT8,
-  publisher_id INT8 REFERENCES games(id) ON DELETE CASCADE
+  publisher_id INT8 REFERENCES games(id) ON DELETE CASCADE,
+  genre_id INT8 REFERENCES genres(id) ON DELETE CASCADE,
+  cost_price DECIMAL(8, 2),
+  sell_price DECIMAL(8, 2),
+  url VARCHAR(255)
 );
